6d81b21de20c0e38a67b4e50d63f8707ac04cbe2
waltercoan <walter.coan@gmail.com>
2015-11-27 18:28:02 -0200

    correção da logica de calculo da frequencia

--- code_arduino.ino
+++ code_arduino.ino
@@ -8,11 +8,11 @@ long timeArray[sizeData];
 AcceleroMMA7361 accelero;
 int x;
 int count = 0;
-int lastMax=0;
-int lastMin=9999;
 long timeIni=0, timeEnd;
 double freqacc=0, freqmed=0;
 int freqcount = 0;
+double freq = 0;
+int actual=0, last =0;
 
 void getWave(){
     timeArray[count] = millis();
@@ -20,39 +20,29 @@ void getWave(){
     if (count >= sizeData){
         count = 0;
         for(int i=0;i<sizeData;i++){
-            if(lastMax < analogArray[i]){
-                lastMax = analogArray[i];
-                if(analogArray[i+1] < lastMax){
-                    if(timeIni == 0){
-                        timeIni = timeArray[i];
-                    }else{
-                        timeEnd = timeArray[i];
-                        double freq = 1000.0 / (timeEnd - timeIni);
+            if(analogArray[i] == 0){
+                if(timeIni == 0){
+                    timeIni = timeArray[i];
+                }else{
+                    timeEnd = timeArray[i];  
+                    if((timeEnd - timeIni) > 10){
+                        freq = 1.0 / ((timeEnd - timeIni) / 1000.0);
                         freqcount++;
                         freqacc = freqacc + freq;
-                        timeIni = 0;
-                        timeEnd = 0;
-                        lastMax = 0;
-                        lastMin = 9999;
                     }
+                    timeEnd = 0;
+                    timeIni = 0;
                 }
             }
-            if(lastMin > analogArray[i]){
-                lastMin = analogArray[i];
-                if(analogArray[i+1] > lastMin){
-                    lastMax = 0;
-                    lastMin = 9999;
-                }
-            }    
         }
-        if(freqcount >= 50){
+        if(freqcount >= 2){
+            delay(100);          
             freqmed = freqacc / freqcount;
             freqacc = 0;
             freqcount = 0;
-            Serial.print("Freq Media: ");
+            Serial.print("Freq: ");
             Serial.print(freqmed);               
             Serial.println(" Hz");
-            //MsTimer2::stop();
         }
     }
 }
@@ -64,7 +54,7 @@ void setup()
   accelero.setARefVoltage(3.3);                   //sets the AREF voltage to 3.3V
   accelero.setSensitivity(LOW);                   //sets the sensitivity to +/-6G
   accelero.calibrate();
-  Serial.println("Calibado...");
+  Serial.println("Calibrado...");
   MsTimer2::set(1, getWave);
   MsTimer2::start();  
   Serial.println("Monitorando...");
@@ -72,28 +62,4 @@ void setup()
 
 void loop()
 {
-     if(Serial.available()){
-       char in = Serial.read();
-       if(in == 'r'){
-           Serial.println("Begin...");
-       }else{
-           if(in == 'p'){
-               
-           }else{
-               if(in == 'd'){
-                   for(int i=0;i<sizeData;i++){
-                       Serial.print(analogArray[i]);
-                       Serial.print(";");
-                   }
-                   Serial.println("");
-                   for(int i=0;i<sizeData;i++){
-                       Serial.print(timeArray[i]);
-                       Serial.print(";");
-                   }                 
-               }
-           }
-       }
-       
-   }
-
 }
